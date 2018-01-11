39b41efdf475fba7c491b440e7faf0e53495d776
lw244130 <lw244130@LR00905.southeast.edu>
2018-01-11 14:03:42 -0600

    finished all before commits

--- index.php
+++ index.php
@@ -10,34 +10,79 @@
 //      e.g. if this is entered in the address bar http://localhost/SeasonalWorkIt/index.php?month=12
 //           the page would display the styling for Winter
 
+$month_from_url = filter_input(INPUT_GET, 'month', FILTER_VALIDATE_INT);
+
 $seasons = array(
     "Winter" => array('bg_color' => 'gray', "text_color" => "white", "saying" => ", happy holidays!"),
     "Spring" => array('bg_color' => 'green', "text_color" => "purple", "saying" => " the ides of march."),
     "Summer" => array('bg_color' => 'yellow', "text_color" => "pink", "saying" => " to wear sunscreen."),
-    "Fall" => array('bg_color' => 'orange', "text_color" => "brown", "saying" => " oktoberfest, Prosit!")
+    "Fall" => array('bg_color' => 'orange', "text_color" => "brown", "saying" => " oktoberfest, Prosit!"),
+    "Error" => array('bg_color' => 'red', "text_color" => "deeppink", "saying" => " dude what did you do?")
 );
+
+if (isset($month_from_url)) {
+    $month = $month_from_url;
+} else {
+    $month = date('n');
+}
+
+
+switch ($month) {
+    case 1:
+    case 2:
+    case 12:
+        $season = 'Winter';
+        break;
+    case 3:
+    case 4:
+    case 5:
+        $season = 'Spring';
+        break;
+    case 6:
+    case 7:
+    case 8:
+        $season = 'Summer';
+        break;
+    case 9:
+    case 10:
+    case 11:
+        $season = 'Fall';
+        break;
+    default:
+       $season = 'Error';
+            break;
+}
+
+//$season = 'Summer';
+
+var_dump($seasons[$season]);
 ?>
 
 <html>
     <head>
         <meta charset="UTF-8">
         <style>body {
-                background-color: cornsilk; }
+                background-color: <?php echo $seasons[$season]['bg_color']?>; }
 
             h1, h2 {
-                color: deeppink; }
+                color: <?php echo $seasons[$season]['text_color']?>; }
         </style>
 
         <title>Seasons Greetings</title>
     </head>
     <body>
         <h1> Season's Greetings! </h1>
-        <h2></h2>
-        <p> I hope you're having a happy DAY </p>
+        <h2>Month: <?php echo $month?></h2>
+        <p> I hope you're having a happy <?php echo $season?> </p>
 
-        <p> and make sure to remember ...? </p>
+        <p> and make sure to remember <?php echo $seasons[$season]['saying']?> </p>
 
         <br><br>
-        <a href="index.php?month=12">Month override test link</a>
+        <?php 
+            for($i = 1; $i <= 12; $i++){
+                echo "<a href=\"index.php?month=".$i."\">Month " . $i . " override test link</a><br>";
+            }
+        ?>
+        
     </body>
 </html>
